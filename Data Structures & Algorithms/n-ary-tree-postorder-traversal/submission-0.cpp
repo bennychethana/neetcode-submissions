0e439ffd83e1aa1c900cf57a98d6a6f7b9cda725
Chethana Benny <benny.c@northeastern.edu>
2026-07-05 20:10:13 -0400

    Add: n-ary-tree-postorder-traversal - submission-0

---
+++ Data Structures & Algorithms/n-ary-tree-postorder-traversal/submission-0.cpp
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> ans;
+    void f(Node* root){
+        if(!root) return;
+        int l = root->children.size();
+        for(int i=0;i<l;i++){
+            f(root->children[i]);
+        }
+        ans.push_back(root->val);
+    }
+    vector<int> postorder(Node* root) {
+        f(root);
+        return ans;
+    }
+};
