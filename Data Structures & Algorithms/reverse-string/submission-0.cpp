56236b3856b1d032b192edaea503c8fbdee343b7
Chethana Benny <benny.c@northeastern.edu>
2026-07-06 19:21:30 -0400

    Add: reverse-string - submission-0

---
+++ Data Structures & Algorithms/reverse-string/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        int l = 0;
+        int r = n-1;
+        while(l<r){
+            char t = s[r];
+            s[r] = s[l];
+            s[l] = t;
+            l++;
+            r--;
+        }
+    }
+};
