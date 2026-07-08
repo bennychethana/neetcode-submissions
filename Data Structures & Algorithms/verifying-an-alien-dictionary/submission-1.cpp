c4001b738ef67298087eb0e27200d6eb1aeb48e9
Chethana Benny <benny.c@northeastern.edu>
2026-07-08 15:12:38 -0400

    Add: verifying-an-alien-dictionary - submission-1

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-1.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        int n = words.size();
+        bool ans = true;
+        unordered_map<char,int> map;
+        for(int i=0;i<order.size();i++){
+            map[order[i]] = i;
+        }
+        bool ok = false;
+        for(int i=1;i<n;i++){
+            int prev_len = words[i-1].size();
+            int cur_len = words[i].size();
+            int l = min(prev_len,cur_len);
+            int it = 0;
+            ok = false;
+            while(it<l){
+                if(map[words[i-1][it]]<map[words[i][it]]){
+                    ok = true;
+                    break;
+                }
+                else if(map[words[i-1][it]]>map[words[i][it]]){
+                    ans = false;
+                    break;
+                }
+                it++;
+            }
+            if(!ok && ans && words[i-1].size()>words[i].size()) return false;
+            if(!ans) return false;
+        }
+        return ans;
+    }
+};
